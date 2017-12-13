0480e4e578b907c7aefe3c155b89239c8195daa0
Rahul Lole <Rahul.Lole@hitachiconsulting.com>
2017-12-13 20:30:06 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AuthService } from "./auth/auth.service";
+import { CallbackComponent } from './pages/callback/callback.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    CallbackComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [Title, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/auth/auth.config.ts
@@ -0,0 +1,19 @@
+
+// src/app/auth/auth.config.ts
+import { ENV } from './../core/env.config';
+
+interface AuthConfig {
+  CLIENT_ID: string;
+  CLIENT_DOMAIN: string;
+  AUDIENCE: string;
+  REDIRECT: string;
+  SCOPE: string;
+};
+
+export const AUTH_CONFIG: AuthConfig = {
+  CLIENT_ID: 'HSBrZ3Oi2t6AAA8G1I9SBtvKfjvEq6mH',
+  CLIENT_DOMAIN: 'ral.auth0.com/',
+  AUDIENCE: 'http://localhost:8083/api/', // likely http://localhost:8083/api/
+  REDIRECT: `${ENV.BASE_URI}/callback`,
+  SCOPE: 'openid profile'
+};
