bf4f21f3993ab7a4b574310a90575fe57239e7b4
Rahul <lole.rahul@gmail.com>
2017-12-13 20:41:46 +0530

    Delete config.js

--- server/config.js
+++
@@ -1,6 +0,0 @@
-// server/config.js
-module.exports = {
-  AUTH0_DOMAIN: 'ral.auth0.com/', // e.g., kmaida.auth0.com
-  AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://mean:mymeanpassword@ds019786.mlab.com:19786/meandb' //mongodb://[USER]:[PASSWORD]@[DS######].mlab.com:[PORT]/[DB_NAME]
-};
